4290d4a7a328da4c10c01b9bb5edcfee60329f57
Ryan Shaw-Harrison <srazash@protonmail.com>
2024-11-22 16:21:51 +0000

    Started work Generate() function

--- gollama/request/request.go
+++ gollama/request/request.go
@@ -1,11 +1,31 @@
 package request
 
-import "errors"
+import (
+	"aigotestapp/gollama/settings"
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
+)
 
 type Request struct {
 	Model  string
 	Prompt string
 	Suffix string
+}
+
+type Response struct {
+	Model                string
+	Created_At           time.Time
+	Response             string
+	Done                 bool
+	Context              []int
+	Total_Duration       int
+	Load_Duration        int
+	Prompt_Eval_Count    int
+	Prompt_Eval_Duration int
+	Eval_Count           int
+	Eval_Duration        int
 }
 
 func Init(model string, prompt string, suffix string) (*Request, error) {
@@ -17,4 +37,31 @@ func Init(model string, prompt string, suffix string) (*Request, error) {
 		Prompt: prompt,
 		Suffix: suffix,
 	}, nil
+}
+
+func (r *Request) Generate(ch chan<- string, settings *settings.Settings) error {
+	server := settings.GetConnectionString()
+	url := fmt.Sprintf("http://%s/api/generate", server)
+
+	request, err := http.NewRequest(http.MethodPost, url, nil)
+	if err != nil {
+		return err
+	}
+
+	request.Header.Add("model", r.Model)
+	request.Header.Add("prompt", r.Prompt)
+	request.Header.Add("suffix", r.Suffix)
+	request.Header.Add("format", "json")
+
+	client := &http.Client{}
+
+	response, err := client.Do(request)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	// TODO
+
+	return nil
 }
