1d7698c7f1c30075645e00e0cd86426a78335158
Ryan Shaw-Harrison <srazash@protonmail.com>
2024-11-22 10:46:10 +0000

    Modified KB/MB/GB calculation to match what ollama shows

--- gollama/models/util.go
+++ gollama/models/util.go
@@ -1,13 +1,13 @@
 package models
 
 func ToKB(b int) float64 {
-	return float64(b) / 1024
+	return float64(b) / 1000
 }
 
 func ToMB(b int) float64 {
-	return float64(b) / 1024 / 1024
+	return float64(b) / 1000 / 1000
 }
 
 func ToGB(b int) float64 {
-	return float64(b) / 1024 / 1024 / 1024
+	return float64(b) / 1000 / 1000 / 1000
 }
